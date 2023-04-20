53535dc6dfd2384d59a4b015119649ae6f582421
ravnkilde1995 <ravnkilde1995@gmail.com>
2023-04-20 14:05:59 +0200

    added table to showcase different books from api

--- CA2_FRONTEND_3SEM/src/components/Book.jsx
+++ CA2_FRONTEND_3SEM/src/components/Book.jsx
@@ -1,47 +1,63 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
 import facade from "../apiFacade";
+import Table from "react-bootstrap/Table";
 
-function Content(){
-    const [dataFromServer, setDataFromServer] = useState("Loading...")
-    let books = []
-    useEffect(() => { 
-        facade.fetchDataGoogle().then(res => {
-            // console.log(res);
-            console.log(res.items);
-            if(res.items){
-              res.items.map((item)=>{
-                books.push(item);
-              })
-            }
-            
-            console.log("liste ", books)
-          setDataFromServer(res.msg)});
-      },[])
-  
-    return (
-      <div>
-        <h1>Books</h1>
-        <h3>{dataFromServer}</h3>
-        {books.length < 0 ? 
-        
-        <div>
-          <h1>ikke nogen bøger</h1>
-        </div>
-        : <>
-        <h1> der er bøger </h1>
-        {books.map((item)=>{
-          console.log("hello hello", item)
-          return(
-            <>
-            <p>{item}</p>
-            </>
-          )
-        })}
-        
-        </>
+function Content() {
+  const [dataFromServer, setDataFromServer] = useState("Loading...");
+  // Bog data gemmes på en liste med useState
+  const [bookList, setBookList] = useState([]);
+
+  useEffect(() => {
+    facade.fetchDataGoogle().then((res) => {
+      //Hvis fetch respsonse har data, tilføjes det til bookList med setBookList
+      if (res.items) {
+        setBookList(res.items);
       }
-        
-      </div>
-    )
-  }
+      //console.log("I useEffect ", bookList);
+      setDataFromServer(res.msg);
+    });
+  }, []);
+
+  return (
+    <div>
+      <br></br>
+      <h1>Books</h1>
+      <h3>{dataFromServer}</h3>
+
+      {/*Vi mapper hvert item vi har fetchet */}
+
+      {bookList.map((item) => {
+        //console.log("hello hello", item);
+        //console.log("Nummer 2", item.id);
+        //console.log("Nummer 3", item.etag);
+
+        return (
+          <>
+            <br />
+            <Table bordered hover>
+              <thead>
+                <tr>
+                  <th style={{ width: "20%" }}>Title</th>
+                  <th style={{ width: "12%" }}>Author</th>
+                  <th style={{ width: "12%" }}>Category</th>
+                  <th style={{ width: "50%" }}>Description</th>
+                  <th style={{ width: "6%" }}>Rating</th>
+                </tr>
+              </thead>
+              <tbody key={item.id}>
+                <tr>
+                  <td>{item.volumeInfo.title}</td>
+                  <td>{item.volumeInfo.authors}</td>
+                  <td>{item.volumeInfo.categories}</td>
+                  <td>{item.volumeInfo.description}</td>
+                  <td>{item.volumeInfo.averageRating}</td>
+                </tr>
+              </tbody>
+            </Table>
+          </>
+        );
+      })}
+    </div>
+  );
+}
 export default Content;
